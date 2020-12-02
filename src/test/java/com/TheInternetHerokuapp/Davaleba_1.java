eaac61cb96580e88c9189ca3d8b7afeff82670aa
Davit Vardiashvili <davit.vardiashvili.1@btu.edu.ge>
2020-12-02 22:07:22 +0400

    Update Davaleba_1.java

--- src/test/java/com/TheInternetHerokuapp/Davaleba_1.java
+++ src/test/java/com/TheInternetHerokuapp/Davaleba_1.java
@@ -1,33 +1,38 @@
 package com.TheInternetHerokuapp;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
-
 public class Davaleba_1 {
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+    @After
+    public void teardown() {
+            driver.quit();
+    }
+
     @Test
-    public void davaleba_1() throws InterruptedException {
-
-        System.setProperty("webdriver.chrome.driver","/Users/DADA.VARDO-PC/Desktop/chromedriver/chromedriver.exe");
-
-        ChromeDriver driver = new ChromeDriver();
-
+    public void test() {
         driver.get("http://the-internet.herokuapp.com");
-        TimeUnit.SECONDS.sleep(2);
-
         driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[20]/a")).click();
-        TimeUnit.SECONDS.sleep(2);
-
         WebElement email = driver.findElement(By.id("email"));
-
         email.sendKeys("Dato.vardiashvili2001@gmail.com");
-
         driver.findElement(By.id("form_submit")).click();
-        TimeUnit.SECONDS.sleep(2);
-
-        driver.quit();
     }
 }
+
